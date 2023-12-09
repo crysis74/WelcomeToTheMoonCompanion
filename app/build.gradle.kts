7f14a546676254349e36da650647e1e92b7c332c
Evgeniy Batalov <evgeniy.batalov@gs-labs.ru>
2023-12-09 13:53:11 +0300

    Update androidx libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,14 +16,6 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    signingConfigs {
-        create("release") {
-            storeFile = file(keystoreProperties["storeFile"] as String)
-            storePassword = keystoreProperties["storePassword"] as String
-            keyAlias = keystoreProperties["keyAlias"] as String
-            keyPassword = keystoreProperties["keyPassword"] as String
-        }
-    }
     namespace = "ru.bepis.mooncompanion"
     compileSdk = 34
 
@@ -35,6 +27,15 @@ android {
         versionName = "1.1"
         vectorDrawables {
             useSupportLibrary = true
+        }
+    }
+
+    signingConfigs {
+        create("release") {
+            storeFile = file(keystoreProperties["storeFile"] as String)
+            storePassword = keystoreProperties["storePassword"] as String
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
         }
     }
 
@@ -67,18 +68,17 @@ android {
 }
 
 dependencies {
-    val koinVersion = "3.4.0"
-    val navVersion = "2.5.3"
-    val fragmentVersion = "1.5.6"
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
-    implementation("androidx.activity:activity:1.7.1")
-    implementation("androidx.core:core-ktx:1.10.0")
+    val navVersion = "2.7.5"
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+    implementation("androidx.activity:activity-ktx:1.8.1")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("com.google.android.material:material:1.7.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("io.insert-koin:koin-android:$koinVersion")
-    implementation("io.insert-koin:koin-androidx-navigation:$koinVersion")
-    implementation("androidx.fragment:fragment-ktx:$fragmentVersion")
+    implementation(platform("io.insert-koin:koin-bom:3.5.1"))
+    implementation("io.insert-koin:koin-android")
+    implementation("io.insert-koin:koin-androidx-navigation")
+    implementation("androidx.fragment:fragment-ktx:1.6.2")
     implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
     implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
     implementation("com.github.kirich1409:viewbindingpropertydelegate-noreflection:1.5.8")
