5dffdb9618ac58b60fe468b1e4780e6df0e0c197
Evgeniy Batalov <evgeniy.batalov@gs-labs.ru>
2023-12-10 21:43:08 +0300

    Update app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "ru.bepis.mooncompanion"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
         vectorDrawables {
             useSupportLibrary = true
         }
